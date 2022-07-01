b7070efbb16e4e9fc6da47da9f2214af7affbfd9
h3rby7 <herbielutz@gmx.net>
2022-07-01 15:51:47 +0200

    added library to convert structs/maps, enhance test, wrote custom date parser to handle slack input format

--- model/eventChannel.go
+++ model/eventChannel.go
@@ -3,12 +3,15 @@ package model
 import (
 	"fmt"
 	"time"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
 )
 
 type EventChannel struct {
-	EventType []string
+	EventType []string `mapstructure:"type"`
 	Location  string
-	Date      time.Time
+	Date      time.Time `mapstructure:"-"`
 	Duration  string
 	Customer  string
 	Occasion  string
@@ -17,14 +20,22 @@ type EventChannel struct {
 
 func NewEventChannel(data map[string]interface{}) (EventChannel, error) {
 	e := EventChannel{}
-	str, ok := data["occasion"].(string)
-	if !ok {
-		return e, createError("occasion", "string")
+	err := mapstructure.Decode(data, &e)
+	if err != nil {
+		return e, err
 	}
-	e.Occasion = str
+	dayStr, ok := data["date"].(string)
+	if !ok {
+		return e, errors.New("Invalid 'date'")
+	}
+	timeStr, ok := data["time"].(string)
+	if !ok {
+		return e, errors.New("Invalid 'time'")
+	}
+	date, err := time.Parse("2006-01-02T15:04", fmt.Sprintf("%vT%v", dayStr, timeStr))
+	if err != nil {
+		return e, err
+	}
+	e.Date = date
 	return e, nil
-}
-
-func createError(key string, expectedType string) error {
-	return fmt.Errorf("field '%v' missing or not of type '%v'", key, expectedType)
 }
--- model/eventChannel_test.go
+++ model/eventChannel_test.go
@@ -4,13 +4,43 @@ import "testing"
 
 func createTestInput() map[string]interface{} {
 	testInput := map[string]interface{}{}
+	testInput["type"] = []string{"show"}
+	testInput["location"] = "a stage"
+	testInput["date"] = "1990-04-28"
+	testInput["time"] = "12:03"
+	testInput["duration"] = "until midnight"
+	testInput["customer"] = "a friend"
 	testInput["occasion"] = "teamevent"
+	testInput["wishes"] = "none"
 	return testInput
 }
 
 func TestNewEventChannel(t *testing.T) {
-	_, err := NewEventChannel(createTestInput())
+	res, err := NewEventChannel(createTestInput())
 	if err != nil {
 		t.Error(err)
+	}
+	if res.EventType[0] != "show" {
+		t.Error("Expected EventType to be 'show'")
+	}
+	if res.Occasion != "teamevent" {
+		t.Error("Expected Occasion to be 'teamevent'")
+	}
+	y, m, d := res.Date.Date()
+	if y != 1990 {
+		t.Errorf("Wrong year, expected 1990, but got '%v'", y)
+	}
+	if m != 4 {
+		t.Errorf("Wrong month, expected 4, but got '%v'", m)
+	}
+	if d != 28 {
+		t.Errorf("Wrong day, expected 28, but got '%v'", d)
+	}
+	h, min, _ := res.Date.Clock()
+	if h != 12 {
+		t.Errorf("Wrong hour, expected 12, but got '%v'", h)
+	}
+	if min != 3 {
+		t.Errorf("Wrong minute, expected 3, but got '%v'", min)
 	}
 }
