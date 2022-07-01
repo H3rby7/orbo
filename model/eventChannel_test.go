0d59a721243c6edc2f365a3d668258b541aafb8e
h3rby7 <herbielutz@gmx.net>
2022-07-01 16:27:45 +0200

    more test cases for 'NewEventChannel'

--- model/eventChannel_test.go
+++ model/eventChannel_test.go
@@ -23,9 +23,22 @@ func TestNewEventChannel(t *testing.T) {
 	if res.EventType[0] != "show" {
 		t.Error("Expected EventType to be 'show'")
 	}
+	if res.Location != "a stage" {
+		t.Error("Expected Location to be 'a stage'")
+	}
+	if res.Duration != "until midnight" {
+		t.Error("Expected Duration to be 'until midnight'")
+	}
+	if res.Customer != "a friend" {
+		t.Error("Expected Customer to be 'a friend'")
+	}
 	if res.Occasion != "teamevent" {
 		t.Error("Expected Occasion to be 'teamevent'")
 	}
+	if res.Wishes != "none" {
+		t.Error("Expected Wishes to be 'none'")
+	}
+	// Date Tests
 	y, m, d := res.Date.Date()
 	if y != 1990 {
 		t.Errorf("Wrong year, expected 1990, but got '%v'", y)
@@ -42,5 +55,23 @@ func TestNewEventChannel(t *testing.T) {
 	}
 	if min != 3 {
 		t.Errorf("Wrong minute, expected 3, but got '%v'", min)
+	}
+}
+
+func TestNewEventChannelMultipleTypes(t *testing.T) {
+	input := createTestInput()
+	input["type"] = []string{"show", "workshop"}
+	res, err := NewEventChannel(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(res.EventType) != 2 {
+		t.Error("Expected EventType to be of length '2'")
+	}
+	if res.EventType[0] != "show" {
+		t.Error("Expected EventType[0] to be 'show'")
+	}
+	if res.EventType[1] != "workshop" {
+		t.Error("Expected EventType[1] to be 'workshop'")
 	}
 }
